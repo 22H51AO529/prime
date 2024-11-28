76534bfc350846f0dc290fa3136efc28b037701a
Rakshitha Kailasa <kailasarakshitha@gmail.com>
2024-11-28 10:37:40 +0530

    initial commit

---
+++ Prime.java
@@ -0,0 +1,22 @@
+class Prime{
+    public static void main(String[] args) {
+        int limit=20;
+        for(int i=2;i<=limit;i++)
+        {
+            boolean isPrime=true;
+            for(int j=2;j<=Math.sqrt(i);j++)
+            {
+                if(i%j==0)
+                {
+                    isPrime=false;
+                    break;
+
+                }
+            }
+            if(isPrime)
+            {
+                System.out.println(i+" ");
+            }
+        }
+    }
+}
